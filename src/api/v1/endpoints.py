0fed31a21efab0d3b814bcb355169933b9d17ec8
olazo-johnalbert <jaolazo4@gmail.com>
2026-04-24 00:40:22 +0800

    feat(api): expose ocr and cor endpoints in api v1

--- src/api/v1/endpoints.py
+++ src/api/v1/endpoints.py
@@ -1,6 +1,10 @@
+from src.schemas.ocr import CORResponse
 from fastapi import APIRouter, HTTPException, Depends
 from src.schemas.prediction import ClassificationRequest, ClassificationResponse
+from src.schemas.ocr import OCRResponse
 from src.services.classifier import ClassifierService
+from src.services.ocr import OCRService
+from fastapi import UploadFile, File
 import logging
 
 logger = logging.getLogger(__name__)
@@ -11,6 +15,9 @@ router = APIRouter()
 def get_classifier_service():
     return ClassifierService()
 
+def get_ocr_service():
+    return OCRService()
+
 @router.post("/classify", response_model=ClassificationResponse)
 async def post_predict(
     request: ClassificationRequest,
@@ -20,11 +27,48 @@ async def post_predict(
     Endpoint to classify the urgency of a student concern or appointment reason.
     """
     try:
-        result = service.classify(request)
+        # Since service.classify is async
+        result = await service.classify(request)
         return result
     except Exception as e:
-        logger.error(f"[Endpoint] Prediction failed: {str(e)}")
+        logger.error(f"[AI Endpoint] Prediction failed: {str(e)}")
         raise HTTPException(
             status_code=500,
             detail="Internal Server Error during classification"
+        )
+
+@router.post("/ocr", response_model=OCRResponse)
+async def post_ocr(
+    file: UploadFile = File(...),
+    service: OCRService = Depends(get_ocr_service)
+):
+    """
+    Endpoint to perform OCR on a single image or document.
+    """
+    try:
+        result = await service.process_document(file)
+        return result
+    except Exception as e:
+        logger.error(f"[OCR Endpoint] Processing failed: {str(e)}")
+        raise HTTPException(
+            status_code=500,
+            detail=f"Internal Server Error during OCR processing: {str(e)}"
+        )
+
+@router.post("/ocr/cor", response_model=CORResponse)
+async def post_ocr_cor(
+    file: UploadFile = File(...),
+    service: OCRService = Depends(get_ocr_service)
+):
+    """
+    Endpoint to perform OCR on a single image or document.
+    """
+    try:
+        result = await service.process_cor(file)
+        return result
+    except Exception as e:
+        logger.error(f"[OCR Endpoint] Processing failed: {str(e)}")
+        raise HTTPException(
+            status_code=500,
+            detail=f"Internal Server Error during OCR processing: {str(e)}"
         )
